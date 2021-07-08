143fa3215c31052451c337e96f6316a1b0774ec2
st0neshi <imericshi@gmail.com>
2021-07-08 02:29:29 -0400

    add js and change span to button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-document.getElementById("person").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "flex";
+// document.getElementById("login").addEventListener("click", function(){
+//     document.querySelector(".popup").style.display = "flex";
+// })
+
+// document.getElementById("close").addEventListener("click", function(){
+//     document.querySelector(".popup").style.display = "none";
+// })
+
+const openLoginButtons = document.querySelectorAll("[data_login_target]");
+const closeLoginButtons = document.querySelectorAll("[data_close_button]");
+const overlay = document.getElementById("overlay");
+
+openLoginButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const login = document.querySelector(button.dataset.loginTarget)
+        openLogin(login)
+    })
 })
 
-document.getElementById("close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
+closeLoginButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const login = button.closest(".login")
+        closeLogin(login)
+    })
 })
+
+function openLogin(login) {
+    if (login == null) return
+    login.classList.add("active")
+    overlay.classList.add("active")
+}
+
+function closeLogin(login) {
+    if (login == null) return
+    login.classList.remove("active")
+    overlay.classList.remove("active")
+}
